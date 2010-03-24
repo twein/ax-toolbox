6307fcb9a2faba8828194e3fb3e883c61e5945a6
toni.martinez <toni.martinez@localhost>
2010-03-24 17:10:15 +0100

    Common.IO.TRKFile: Implemented ReadLog(...)

--- trunk/Common/IO/TRKFile.cs
+++ trunk/Common/IO/TRKFile.cs
@@ -2,15 +2,90 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using AXToolbox.Common.Geodesy;
 
 
 namespace AXToolbox.Common.IO
 {
-    public static class TRKFile 
+    public static class TRKFile
     {
         public static GPSLog ReadLog(string filePath)
         {
-            throw new NotImplementedException();
+            CoordAdapter ca = null;
+            bool utm = false;
+
+            var log = new GPSLog(filePath);
+            var content = from line in File.ReadAllLines(filePath)
+                          where line.Length > 0
+                          select line;
+
+            foreach (var line in content)
+            {
+                var fields = line.Split(' ');
+                switch (line[0])
+                {
+                    case 'G':
+                        //Datum
+                        log.Datum = "WGS84";
+                        var datum = line.Substring(2).Trim();
+                        if (datum == "WGS 84") //Dirty hack!!!
+                            datum = "WGS84";
+                        ca = new CoordAdapter(datum, log.Datum);
+                        break;
+                    case 'U':
+                        //Coordinate units
+                        utm = (fields[1] == "0");
+                        break;
+                    case 'P':
+                        //Logger info
+                        log.LoggerModel = line.Substring(2).Trim();
+                        break;
+                    case 'T':
+                        //Track point
+                        var fix = new GPSFix();
+                        fix.Time = DateTime.Parse(fields[4] + " " + fields[5]);
+                        fix.GpsAltitude = double.Parse(fields[7]);
+                        if (utm)
+                        {
+                            //utm coordinates
+                            //convert to wgs84 latlon
+                            var llp = ca.ConvertToLatLong(new UTMPoint()
+                            {
+                                Zone = fields[1],
+                                Easting = double.Parse(fields[2]),
+                                Northing = double.Parse(fields[3]),
+                                Altitude = fix.GpsAltitude
+                            });
+                            fix.Latitude = llp.Latitude;
+                            fix.Longitude = llp.Longitude;
+                        }
+                        else
+                        {
+                            //latlon coordinates
+                            //convert to wgs84
+                            var latitude = fields[2].Split('º');
+                            var longitude = fields[3].Split('º');
+                            var llp = ca.ConvertToLatLong(new LatLongPoint()
+                            {
+                                Latitude = double.Parse(latitude[0]) * (latitude[1] == "S" ? -1 : 1),
+                                Longitude = double.Parse(longitude[0]) * (longitude[1] == "W" ? -1 : 1),
+                                Altitude = fix.GpsAltitude
+                            });
+                            fix.Latitude = llp.Latitude;
+                            fix.Longitude = llp.Longitude;
+                        }
+                        log.Track.Add(fix);
+                        break;
+                    //case 'L':
+                    //    //Timezone
+                    //    var tz = TimeZoneInfo.CreateCustomTimeZone("x", -TimeSpan.Parse(fields[1]), "", "");
+                    //    break;
+
+                }
+            }
+
+            return log;
         }
 
         public static void Export(string filePath, List<Point> track, string datum, string utmZone)
@@ -18,8 +93,8 @@ namespace AXToolbox.Common.IO
             var sw = new StreamWriter(filePath, false);
 
             sw.WriteLine("G {0}", datum);
-            //sw.WriteLine("L {0}", UTCOffset.ToString());
-            sw.WriteLine("C 255 0 0 3");  //TODO: PointList.SaveTrack: Set goals color & thickness
+            sw.WriteLine("U 0"); //UTM
+            sw.WriteLine("C 255 0 0 3");  //TODO: PointList.SaveTrack: Set track color & thickness
             bool first = true;
             foreach (var point in track)
             {
--- trunk/Common/IO/WPTFile.cs
+++ trunk/Common/IO/WPTFile.cs
@@ -11,68 +11,6 @@ namespace AXToolbox.Common.IO
         public static List<Waypoint> ReadWaypoints(string filePath)
         {
             throw new NotImplementedException();
-            /*
-            /// <summary>Loads a List<Point> from a CompeGPS goals file</summary>
-            /// <param name="filePath"></param>
-            /// <param name="datum"></param>
-            /// <returns></returns>
-            static public List<Point> GetTrack(string filePath, string datum, string UTMZone)
-            {
-                return LoadTRKFile(filePath, DateTime.Parse("0001/01/01"), DateTime.Parse("9999/01/01"), datum, UTMZone);
-            }
-
-            /// <summary>Loads a List<Point> from a CompeGPS goals file
-            /// It only includes the points between mintime and maxtime</summary>
-            /// <param name="filePath"></param>
-            /// <param name="minTime"></param>
-            /// <param name="maxTime"></param>
-            /// <param name="datum"></param>
-            /// <returns></returns>
-            static public List<Point> LoadTRKFile(string filePath, DateTime minTime, DateTime maxTime, string datum, string UTMZone)
-            {
-                string line;
-                string[] fields;
-
-                List<Point> points = new List<Point>();
-
-                Point p;
-                StreamReader sr = File.OpenText(filePath);
-                while ((line = sr.ReadLine()) != null)
-                {
-                    line = Regex.Replace(line, "\\s+", " ");//replace multiple spaces/tabs for a single space
-                    line = Regex.Replace(line, "^\\s+", "");//remove spaces at the beginning of line
-
-                    fields = line.Split(" ".ToCharArray());
-                    switch (fields[0])
-                    {
-                        case "T":
-                            p = new Point(
-                                fields[1],
-                                NumberParser.Parse(fields[2]),
-                                NumberParser.Parse(fields[3]),
-                                NumberParser.Parse(fields[7]),
-                                DateTime.Parse(fields[4] + " " + fields[5]));
-                            if (p.Time >= minTime && p.Time <= maxTime)
-                            {
-                                if (p.Zone != UTMZone)
-                                    throw new InvalidOperationException("Wrong goals UTM zone: " + p.Zone);
-                                points.Add(p);
-                            }
-                            break;
-                        case "G":
-                            if (line.Substring(2) != datum)
-                                throw new InvalidOperationException("Wrong goals datum: " + line.Substring(2));
-                            break;
-                        //case "L":
-                        //	UTCOffset=TimeSpan.Parse(fields[1]);
-                        //	break;
-                    }
-                }
-                sr.Close();
-
-                return points;
-            }
-            */
         }
 
         static public void Export(string filePath, List<Waypoint> waypoints, string datum, string utmZone)
