ceb0ec976f699e91f063aea0b31b518ff1796896
toni.martinez <toni.martinez@localhost>
2010-03-25 13:05:00 +0100

    Added Tests project ouline.

---
+++ trunk/Tests/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AXToolbox.Tests
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
