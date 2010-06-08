bac382ae724a1e9551bbd3f7adca6081e77985f0
toni.martinez <toni.martinez@localhost>
2010-06-08 21:53:26 +0200

    Bugfixes

--- trunk/Common/Types/Point.cs
+++ trunk/Common/Types/Point.cs
@@ -17,7 +17,8 @@ namespace AXToolbox.Common
 
         public override string ToString()
         {
-            return string.Format("{0:0000}/{1:0000} {2:#.} {3:HH:mm:ss}", ((Easting % 100000) / 10.0), ((Northing % 100000) / 10.0), Altitude, Time);
+            //return string.Format("{3:HH:mm:ss} {0:0000}/{1:0000} {2:#.}", ((Easting % 100000) / 10.0), ((Northing % 100000) / 10.0), Altitude, Time);
+            return string.Format("{0:HH:mm:ss} {1} {2:000000} {3:0000000} {4:0}", Time, Zone, Easting, Northing, Altitude);
         }
     }
 }
