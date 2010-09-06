e9599595c36383ac13b665c10a90208b573f25cb
toni <toni.martinez@gmail.com>
2010-09-06 15:57:54 +0200

    Added validation rule to Settings ReferencePoint

---
+++ trunk/Model/Validation/PointValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using AXToolbox.Common;
+using System.Collections.Generic;
+
+namespace AXToolbox.Model.Validation
+{
+    public class PointValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Point point;
+            if (Point.TryParse((string)value, out point))
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                return new ValidationResult(false, "Value is not a valid point");
+            }
+        }
+    }
+}
