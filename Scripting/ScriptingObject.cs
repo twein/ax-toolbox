7b6ac40f064b53d67cf6c13e2101210c56f8a144
toni.martinez <toni.martinez@localhost>
2010-10-25 22:04:44 +0200

    More scripting documentation. A little scripting code.

--- Scripting/ScriptingObject.cs
+++ Scripting/ScriptingObject.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using AXToolbox.Common;
 using AXToolbox.MapViewer;
+using System.Text.RegularExpressions;
+using System;
 
 namespace AXToolbox.Scripting
 {
@@ -10,18 +12,48 @@ namespace AXToolbox.Scripting
         protected string name;
         protected string type;
         protected string[] parameters;
-        protected FlightSettings settings;
+        protected string displayMode;
+        protected string[] displayParameters;
 
-        public ScriptingObject(string name, string type, string[] parameters, FlightSettings settings)
+        public ScriptingObject(string name, string type, string[] parameters, string displayMode, string[] displayParameters)
         {
             this.name = name;
             this.type = type;
             this.parameters = parameters;
-            this.settings = settings;
+            this.displayMode = displayMode;
+            this.displayParameters = displayParameters;
         }
 
         public abstract void Resolve(FlightReport report);
 
-        public abstract void Display(MapViewerControl map);
+        public abstract MapOverlay Display();
+
+        public static double ParseAltitude(string str)
+        {
+            double altitude = 0;
+
+            str = str.Trim().ToLower();
+            var regex = new Regex(@"(?<value>\d|\.+)\s*(?<units>\w*)");
+            var matches = regex.Matches(str);
+            if (matches.Count != 1)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                altitude = double.Parse(matches[0].Groups["value"].Value);
+                var units = matches[0].Groups["units"].Value;
+                if (units == "ft" || units == "")
+                {
+                    altitude *= 0.3048;
+                }
+                else if (units != "m")
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+            return altitude;
+        }
     }
 }
