dcbe6a5faa87fa677345d46f815be67701142434
Toni" <toni at balloonerds.com@localhost>
2010-10-29 12:41:01 +0200

    Syntax checking in scripting.

--- Scripting/ScriptingTask.cs
+++ Scripting/ScriptingTask.cs
@@ -2,14 +2,23 @@
 using System.Globalization;
 using AXToolbox.Common;
 using AXToolbox.MapViewer;
+using System.Collections.Generic;
 
 namespace AXToolbox.Scripting
 {
     public class ScriptingTask : ScriptingObject
     {
+        private static readonly List<string> taskTypes = new List<string>
+        {
+            "PDG","JDG","HWZ","FIN","FON","HNH","WSD","GBM","CRT","RTA","ELB","LRN","MDT","SFL","MDD","XDT","XDI","XDD","ANG","3DT"
+        };
+
         internal ScriptingTask(string name, string type, string[] parameters, string displayMode, string[] displayParameters)
             : base(name, type, parameters, displayMode, displayParameters)
-        { }
+        {
+            if (!taskTypes.Contains(type))
+                throw new ArgumentException("Unknown task type '" + type + "'");
+        }
 
         public override void Run(FlightReport report)
         {
