9647099bb2bc586c4b07ee5a3c8e8921cf976527
Toni" <toni at balloonerds.com@localhost>
2010-11-05 16:40:18 +0100

    More syntax checking in scripting.

--- Scripting/ScriptingArea.cs
+++ Scripting/ScriptingArea.cs
@@ -18,16 +18,40 @@ namespace AXToolbox.Scripting
         };
 
 
+        protected Point centerPoint = null;
+        protected double radius = 0;
+
         internal ScriptingArea(string name, string type, string[] parameters, string displayMode, string[] displayParameters)
             : base(name, type, parameters, displayMode, displayParameters)
+        { }
+
+        public override void CheckConstructorSyntax()
         {
             if (!types.Contains(type))
                 throw new ArgumentException("Unknown area type '" + type + "'");
 
+            var engine = ScriptingEngine.Instance;
+            switch (type)
+            {
+                case "CIRCLE":
+                    if (parameters.Length != 2)
+                        throw new ArgumentException("Syntax error in circle definition");
+                    if (!engine.Heap.ContainsKey(parameters[0]))
+                        throw new ArgumentException("Undefined point " + parameters[0]);
+                    ParseLength(parameters[1]);
+                    break;
+                case "POLY":
+                    if (parameters.Length != 1)
+                        throw new ArgumentException("Syntax error in polygon definition");
+                    break;
+            }
+        }
+
+        public override void CheckDisplayModeSyntax()
+        {
             if (!displayModes.Contains(displayMode))
                 throw new ArgumentException("Unknown display mode '" + displayMode + "'");
-
-            //TODO: more syntax checking
+            //TODO: syntax checking
         }
 
         public override void Reset()
@@ -36,12 +60,34 @@ namespace AXToolbox.Scripting
 
         public override void Run(FlightReport report)
         {
-            throw new NotImplementedException();
+            var engine = ScriptingEngine.Instance;
+            switch (type)
+            {
+                case "CIRCLE":
+                    var spoint = (ScriptingPoint)engine.Heap[parameters[0]];
+                    if (spoint != null)
+                        centerPoint = spoint.Point;
+                    radius = ParseLength(parameters[1]);
+                    break;
+                case "POLY":
+                    throw new NotImplementedException();
+            }
         }
 
         public override MapOverlay GetOverlay()
         {
-            throw new NotImplementedException();
+            MapOverlay overlay = null;
+            switch (type)
+            {
+                case "CIRCLE":
+                    var center = new System.Windows.Point(centerPoint.Easting, centerPoint.Northing);
+                    overlay = new CircularAreaOverlay(center, radius, name);
+                    break;
+                case "POLY":
+                    throw new NotImplementedException();
+            }
+
+            return overlay;
         }
 
         public override string ToString()
