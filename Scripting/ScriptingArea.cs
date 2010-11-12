374ddd7bb19abbd681c4639d68307a31f318d976
Toni" <toni at balloonerds.com@localhost>
2010-11-12 13:05:27 +0100

    More syntax checking in AREA. More scripting documentation.

--- Scripting/ScriptingArea.cs
+++ Scripting/ScriptingArea.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using AXToolbox.Common;
 using AXToolbox.MapViewer;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AXToolbox.Scripting
 {
@@ -18,40 +19,39 @@ namespace AXToolbox.Scripting
         };
 
 
-        protected Point centerPoint = null;
-        protected double radius = 0;
-
         internal ScriptingArea(string name, string type, string[] parameters, string displayMode, string[] displayParameters)
             : base(name, type, parameters, displayMode, displayParameters)
-        { }
-
-        public override void CheckConstructorSyntax()
         {
             if (!types.Contains(type))
                 throw new ArgumentException("Unknown area type '" + type + "'");
 
+            if (!displayModes.Contains(displayMode))
+                throw new ArgumentException("Unknown display mode '" + displayMode + "'");
+
             var engine = ScriptingEngine.Instance;
+
             switch (type)
             {
                 case "CIRCLE":
-                    if (parameters.Length != 2)
+                    if (parameters.Length < 2)
                         throw new ArgumentException("Syntax error in circle definition");
+
                     if (!engine.Heap.ContainsKey(parameters[0]))
-                        throw new ArgumentException("Undefined point " + parameters[0]);
+                        throw new ArgumentException("Undefined point '" + parameters[0] + "'");
+
                     ParseLength(parameters[1]);
                     break;
+
                 case "POLY":
-                    if (parameters.Length != 1)
-                        throw new ArgumentException("Syntax error in polygon definition");
+                    if (parameters.Length < 1)
+                        throw new ArgumentException("Syntax error in poly definition");
+
+                    if (!File.Exists(parameters[0]))
+                        throw new ArgumentException("Track file not found '" + parameters[0] + "'");
                     break;
             }
-        }
 
-        public override void CheckDisplayModeSyntax()
-        {
-            if (!displayModes.Contains(displayMode))
-                throw new ArgumentException("Unknown display mode '" + displayMode + "'");
-            //TODO: syntax checking
+            //TODO: more syntax checking
         }
 
         public override void Reset()
@@ -60,34 +60,12 @@ namespace AXToolbox.Scripting
 
         public override void Run(FlightReport report)
         {
-            var engine = ScriptingEngine.Instance;
-            switch (type)
-            {
-                case "CIRCLE":
-                    var spoint = (ScriptingPoint)engine.Heap[parameters[0]];
-                    if (spoint != null)
-                        centerPoint = spoint.Point;
-                    radius = ParseLength(parameters[1]);
-                    break;
-                case "POLY":
-                    throw new NotImplementedException();
-            }
+            throw new NotImplementedException();
         }
 
         public override MapOverlay GetOverlay()
         {
-            MapOverlay overlay = null;
-            switch (type)
-            {
-                case "CIRCLE":
-                    var center = new System.Windows.Point(centerPoint.Easting, centerPoint.Northing);
-                    overlay = new CircularAreaOverlay(center, radius, name);
-                    break;
-                case "POLY":
-                    throw new NotImplementedException();
-            }
-
-            return overlay;
+            throw new NotImplementedException();
         }
 
         public override string ToString()
