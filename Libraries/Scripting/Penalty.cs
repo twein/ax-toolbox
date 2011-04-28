52d8694e0c55ac89d96b3cfb1733208333efb869
Toni" <toni at balloonerds.com@localhost>
2011-04-28 19:17:49 +0200

    Added a result list to main window. Penalty object (not finished). Documentation on penalties (not finished)

---
+++ Libraries/Scripting/Penalty.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AXToolbox.Scripting
+{
+    public enum PenaltyType { Result, TaskPoints, CompetitionPoints, GroupB }
+
+    public class Penalty
+    {
+        public string TaskName { get; protected set; }
+        public string TaskType { get; protected set; }
+        public PenaltyType Type { get; protected set; }
+        public string Unit { get; protected set; } //
+        public double Value { get; protected set; }
+        public string Description { get; protected set; }
+
+        protected Penalty(string taskName, string taskType)
+        {
+            TaskName = taskName;
+            TaskType = taskType;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1:0}{2})", Description, Value, Unit);
+        }
+    }
+}
