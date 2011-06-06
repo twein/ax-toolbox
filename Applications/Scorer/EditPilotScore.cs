3a48cd8b7883d67989dbc8576645619ea06d8d61
Toni <toni@balloonerds.com>
2011-06-06 14:47:56 +0200

    Begin programming the datagrid. Not working.

--- Applications/Scorer/EditPilotScore.cs
+++ Applications/Scorer/EditPilotScore.cs
@@ -1,23 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Scorer
 {
-    [Serializable]
-    public class PilotScore
+    public class EditPilotScore
     {
-        public int TaskNumber { get; set; }
         public int PilotNumber { get; set; }
-        public int Position { get; set; }
+        public string PilotName { get; set; }
+
         public decimal ManualMeasure { get; set; }
         public decimal Measure { get; set; }
         public decimal ManualMeasurePenalty { get; set; }
         public decimal MeasurePenalty { get; set; }
-        public decimal Result { get; set; }
         public int ManualTaskScorePenalty { get; set; }
         public int TaskScorePenalty { get; set; }
         public int ManualCompetitionScorePenalty { get; set; }
         public int CompetitionScorePenalty { get; set; }
-        public int Score { get; set; }
         public string ManualInfringedRules { get; set; }
         public string InfringedRules { get; set; }
     }
--- Applications/Scorer/MainWindow.xaml.cs
+++ Applications/Scorer/MainWindow.xaml.cs
@@ -14,14 +14,32 @@ using System.Windows.Shapes;
 
 namespace Scorer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public List<EditPilotScore> Score { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //http://www.i-programmer.info/programming/wpf-workings/620-using-the-wpf-net-40-datagrid-.html
+            Score = new List<EditPilotScore>();
+            var rnd = new Random();
+            for (var i = 0; i < 90; i++)
+            {
+                var pscore = new EditPilotScore()
+                {
+                    PilotNumber = i + 1,
+                    PilotName = string.Format("Pilot number {0:##0}", i + 1),
+                    ManualMeasure = (decimal)(100 * rnd.NextDouble()),
+                    ManualMeasurePenalty = (decimal)(100 * rnd.NextDouble()),
+                    ManualTaskScorePenalty = (int)(100 * rnd.NextDouble()),
+                    ManualCompetitionScorePenalty = (int)(100 * rnd.NextDouble()),
+                    ManualInfringedRules = "some infringed rule"
+                };
+                Score.Add(pscore);
+            }
+            dgMain.DataContext = Score;
         }
     }
 }
