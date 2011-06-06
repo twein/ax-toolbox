0571da610d741286112fc56d30e4919f70bfca52
Toni" <toni at balloonerds.com@localhost>
2011-06-06 18:23:50 +0200

    Further work on datagrid.

--- Applications/Scorer/EditPilotScore.cs
+++ Applications/Scorer/EditPilotScore.cs
@@ -2,9 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AXToolbox.Scripting;
 
 namespace Scorer
 {
+    public class Result
+    {
+        public ResultType Type { get; set; }
+        public Decimal Value { get; set; }
+
+        public Result(ResultType type)
+        {
+            Type = type;
+        }
+        public Result(decimal value)
+        {
+            Type = ResultType.Result;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            var str = "";
+
+            if (Type == ResultType.No_Flight)
+                str = "NF";
+            else if (Type == ResultType.No_Result)
+                str = "NR";
+            else
+                str = string.Format("{0:#.00}", Value);
+
+            return str;
+        }
+    }
+
+
     public class EditPilotScore
     {
         public int PilotNumber { get; set; }
