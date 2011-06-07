b897c812a5df75191760726ce4750ac3057e4b48
Toni <toni@balloonerds.com>
2011-06-07 14:54:17 +0200

    Result class in a separate file

--- Applications/Scorer/EditPilotScore.cs
+++ Applications/Scorer/EditPilotScore.cs
@@ -1,66 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
+using System.Windows.Data;
 using AXToolbox.Scripting;
 
 namespace Scorer
 {
-    public class Result
-    {
-        public ResultType Type { get; set; }
-        public Decimal Value { get; set; }
-
-        public Result(ResultType type)
-        {
-            Type = type;
-        }
-        public Result(decimal value)
-        {
-            Type = ResultType.Result;
-            Value = value;
-        }
-
-        public override string ToString()
-        {
-            var str = "";
-
-            if (Type == ResultType.No_Flight)
-                str = "NF";
-            else if (Type == ResultType.No_Result)
-                str = "NR";
-            else
-                str = string.Format("{0:#.00}", Value);
-
-            return str;
-        }
-        public static Result Parse(string value)
-        {
-            Result result;
-            decimal tmpResult;
-            var str = value.Trim().ToUpper();
-            if (decimal.TryParse(str, out tmpResult))
-            {
-                result = new Result(tmpResult);
-            }
-            else if (str == "NF")
-            {
-                result = new Result(AXToolbox.Scripting.ResultType.No_Flight);
-            }
-            else if (str == "NR")
-            {
-                result = new Result(AXToolbox.Scripting.ResultType.No_Result);
-            }
-            else
-            {
-                throw new InvalidCastException();
-            }
-
-            return result;
-        }
-    }
-
-
     public class EditPilotScore
     {
         public int PilotNumber { get; set; }
