83c328a3b0097412992ae06b59e4a376312014f5
Toni <toni@balloonerds.com>
2011-06-17 13:43:43 +0200

    EditCollection generic base usercontrol for all the edition pages

--- Applications/Scorer/UserControls/EditTaskResults.xaml.cs
+++ Applications/Scorer/UserControls/EditTaskResults.xaml.cs
@@ -6,26 +6,17 @@ using System.Collections.ObjectModel;
 
 namespace Scorer
 {
-    /// <summary>
-    /// Interaction logic for EditTaskScores.xaml
-    /// </summary>
-    public partial class EditTaskResults : UserControl
+    public partial class EditTaskResults : EditCollection<PilotResult>
     {
-        public ObservableCollection<PilotResult> PilotResults { get; set; }
-
-        public EditTaskResults(Task task)
+        public EditTaskResults(ObservableCollection<PilotResult> taskResults, EditOptions editOptions)
+            : base(taskResults, editOptions)
         {
             InitializeComponent();
-
-            //http://www.i-programmer.info/programming/wpf-workings/620-using-the-wpf-net-40-datagrid-.html
-            PilotResults=new ObservableCollection<PilotResult>();
-            task.PilotResults.CopyTo(PilotResults);
-            dgMain.DataContext = PilotResults;
         }
 
         private void buttonSave_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Save();
         }
     }
 }
