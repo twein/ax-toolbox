7b2efe63e9f14da2519e7d980ebf8ebbd46ff5f0
Toni <Toni@localhost>
2013-02-28 18:08:14 +0100

    New server-neutral publishing

--- Applications/FlightAnalyzer/Properties/AssemblyInfo.cs
+++ Applications/FlightAnalyzer/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyVersion("11.09.21.01")]
-[assembly: AssemblyVersionAttribute("13.3.1.1")]
+[assembly: AssemblyVersionAttribute("13.03.01.1")]
