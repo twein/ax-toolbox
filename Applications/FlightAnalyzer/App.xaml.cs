e724cbcd0c2057aebb6e57be07ea7cb0b8d1a7ef
Toni <toni@balloonerds.com>
2013-03-07 13:53:43 +0100

    Shortcuts to documentation. Added AXS templates.

--- Applications/FlightAnalyzer/App.xaml.cs
+++ Applications/FlightAnalyzer/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Deployment.Application;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using IWshRuntimeLibrary;
 
 //TODO remove AXToolbox from namespace
 namespace AXToolbox.FlightAnalyzer
@@ -13,6 +18,20 @@ namespace AXToolbox.FlightAnalyzer
             if (e.Args != null && e.Args.Length > 0)
             {
                 Properties["FileToOpen"] = e.Args[0];
+            }
+
+            if (ApplicationDeployment.CurrentDeployment.IsFirstRun)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var company = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute), false)).Company;
+
+                var shortcutName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Programs), company, "FlightAnalyzer Documentation.lnk");
+                var targetPath = Path.Combine(Path.GetDirectoryName(assembly.Location), "Documentation");
+
+                var shell = new WshShell();
+                var shortcut = (IWshShortcut)shell.CreateShortcut(shortcutName);
+                shortcut.TargetPath = targetPath;
+                shortcut.Save();
             }
         }
     }
