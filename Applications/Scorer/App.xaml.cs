e39181cd5f3dd0e015460f154c9b3cace9db1a3b
Toni <toni@balloonerds.com>
2013-03-07 13:53:43 +0100

    Shortcuts to documentation. Added AXS templates.

--- Applications/Scorer/App.xaml.cs
+++ Applications/Scorer/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Deployment.Application;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using IWshRuntimeLibrary;
 
 namespace Scorer
 {
@@ -12,6 +17,20 @@ namespace Scorer
             if (e.Args != null && e.Args.Length > 0)
             {
                 Properties["FileToOpen"] = e.Args[0];
+            }
+
+            if (ApplicationDeployment.CurrentDeployment.IsFirstRun)
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var company = ((AssemblyCompanyAttribute)Attribute.GetCustomAttribute(assembly, typeof(AssemblyCompanyAttribute), false)).Company;
+
+                var shortcutName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Programs), company, "Scorer Documentation.lnk");
+                var targetPath = Path.Combine(Path.GetDirectoryName(assembly.Location), "Documentation");
+
+                var shell = new WshShell();
+                var shortcut = (IWshShortcut)shell.CreateShortcut(shortcutName);
+                shortcut.TargetPath = targetPath;
+                shortcut.Save();
             }
         }
     }
